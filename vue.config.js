bf6ef0afec34025402ca9d0905c893fe5da4ebe8
chenzechuang <chen1252961354@qq.com>
2020-03-31 18:13:22 +0800

    feat: add config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-'use strict'
-const path = require('path')
+"use strict";
+const path = require("path");
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
-const name = 'vue-mall' // page title
+const name = "vue-mall"; // page title
 
 module.exports = {
   configureWebpack: {
@@ -14,42 +14,58 @@ module.exports = {
     name: name,
     resolve: {
       alias: {
-        '@': resolve('src')
+        "@": resolve("src")
       }
     }
   },
+
   chainWebpack(config) {
     // set svg-sprite-loader
     config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
+      .rule("svg")
+      .exclude.add(resolve("src/icons"))
+      .end();
     config.module
-      .rule('icons')
+      .rule("icons")
       .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
+      .include.add(resolve("src/icons"))
       .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
       .options({
-        symbolId: 'icon-[name]'
+        symbolId: "icon-[name]"
       })
-      .end()
+      .end();
 
     // set preserveWhitespace
     config.module
-      .rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
       .tap(options => {
-        options.compilerOptions.preserveWhitespace = true
-        return options
+        options.compilerOptions.preserveWhitespace = true;
+        return options;
       })
-      .end()
+      .end();
 
-    config
-      .when(process.env.NODE_ENV === 'development',
-        config => config.devtool('cheap-source-map')
-      )
+    config.when(process.env.NODE_ENV === "development", config =>
+      config.devtool("cheap-source-map")
+    );
+  },
+
+  css: {
+    loaderOptions: {
+      stylus: {
+        "resolve url": true,
+        import: []
+      }
+    }
+  },
+
+  pluginOptions: {
+    "cube-ui": {
+      postCompile: false,
+      theme: false
+    }
   }
-}
+};
